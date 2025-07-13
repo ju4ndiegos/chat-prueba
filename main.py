3eaf6a22bd59f73f2d21361b32c23206e97b59ef
Juan Diego <jd.sanchez2@uniandes.edu.co>
2025-07-13 08:51:12 -0500

    Añadir middleware CORS para permitir solicitudes desde orígenes específicos

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 # --- Expose as a web backend using FastAPI ---
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from modelo import app
 from langchain_core.messages import HumanMessage
 import uvicorn
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://ju4ndiegos.github.io",
+]
+
+
 web_app = FastAPI()
 
+web_app.add_middleware(CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @web_app.post("/chat")
 async def chat_endpoint(request: Request):
     data = await request.json()
